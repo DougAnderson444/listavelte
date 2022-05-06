00b080f9a3c39eb64211e517eedff3151f4a6a9e
Doug A <douganderson444@gmail.com>
2022-05-06 09:50:50 -0300

    rename style sheet to scss

---
+++ src/lib/Styling.svelte
@@ -0,0 +1,8 @@
+<slot />
+
+<!-- global: https://github.com/sveltejs/svelte-preprocess/blob/5db27a3ce4b54a33270320e25b9110f47358b327/docs/preprocessing.md#globalstyle  -->
+
+<!-- @import via postcss-import as defined at ./postcss.config.cjs -->
+<style global lang="postcss">
+	@import './stylesheet.scss';
+</style>
